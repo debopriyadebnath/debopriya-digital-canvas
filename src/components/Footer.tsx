10461a01f0180fdf4f1b8bca1767ee456a6a3c5f
debopriyadebnath <debopriyadebnath121@gmail.com>
2025-08-15 23:36:06 +0530

    Add LeetCode button to footer with SVG icon

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -39,6 +39,17 @@ const Footer = () => {
             >
               <Linkedin size={18} />
             </Button>
+            <Button
+              variant="ghost"
+              size="sm"
+              className="text-muted-foreground hover:text-primary"
+              onClick={() => window.open('https://leetcode.com/u/DebopriyaaaDebnath/', '_blank')}
+            >
+              {/* Simple LeetCode SVG icon */}
+              <svg width="18" height="18" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M16 2C8.268 2 2 8.268 2 16s6.268 14 14 14 14-6.268 14-14S23.732 2 16 2zm0 26C9.373 28 4 22.627 4 16S9.373 4 16 4s12 5.373 12 12-5.373 12-12 12zm-1-17v6h6v2h-8V9h2zm7.707 7.707l-2.414 2.414A1 1 0 0120 19h-8v-2h7.586l2.707-2.707a1 1 0 011.414 1.414z" fill="currentColor"/>
+              </svg>
+            </Button>
           </div>
         </div>
 
